620283d0c9c47f161af6a20823baa5366f437df6
jffifa <jffifa@gmail.com>
2015-03-12 00:12:17 +0800

    modified

--- kotoridb/models.py
+++ kotoridb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from kotoridb import utils
 
 # Create your models here.
 """
@@ -20,6 +21,7 @@ class Anime(models.Model):
     def __str__(self):
         return self.title
 
+
     title = models.CharField(max_length=128, default='title', db_index=True)
     translations = models.CharField(max_length=128, blank=True, default='', db_index=True)
     #translations = models.ManyToManyField(Translation, blank=True)
@@ -39,13 +41,23 @@ class Anime(models.Model):
     studio = models.ForeignKey(Studio, blank=True, null=True, default=None)
     comment = models.TextField(blank=True, default='')
 
+    def save(self, *args, **kwargs):
+        if not self.dom_on_air_tv and self.dom_on_air_link:
+            self.dom_on_air_tv = utils.guess_tv_name(self.dom_on_air_link)
+        super(Anime, self).save(*args, **kwargs)
+
 class OnAirInfo(models.Model):
 
     def __str__(self):
         return self.tv_station+'|'+str(self.time)
 
-    tv_station = models.CharField(max_length=128, default='tv_station')
+    tv_station = models.CharField(max_length=128, blank=True, default='')
     time = models.DateTimeField(blank=True, null=True, default=None)
     link = models.URLField(max_length=256, blank=True, default='')
     anime = models.ForeignKey(Anime)
+
+    def save(self, *args, **kwargs):
+        if not self.tv_station and self.link:
+            self.tv_station = utils.guess_tv_name(self.link)
+        super(OnAirInfo, self).save(*args, **kwargs)
 
