a429cb77795faca020c84cc1611044d855310fbd
jffifa <jffifa@gmail.com>
2015-03-11 23:03:54 +0800

    modified

--- kotoridb/views.py
+++ kotoridb/views.py
@@ -17,15 +17,21 @@ def nav_context():
     }}
     return nav_context
 
+def on_air_time_show(t):
+    if int(t.strftime('%H')) <= 4:
+        pass
+    else:
+        return t.strftime('%H:%M, %')
+
 def on_air(request):
     now = timezone.now()
     td_before = datetime.timedelta(weeks=4)
     animes = Anime.objects.filter(on_air_time__lte=now+td_before).order_by('on_air_time')
-    on_air_animes = []
+    on_air_animes = {w:[] for w in range(7)}
     for a in animes:
         end_time = a.on_air_time + datetime.timedelta(weeks=a.on_air_weeks)
         if end_time > now:
-            on_air_animes.append(a)
+            on_air_animes[int(a.on_air_time.strftime('%w'))].append(a)
 
     context = nav_context()
     context['animes'] = on_air_animes
