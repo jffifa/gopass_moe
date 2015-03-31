2e77f6af90c9617bc84df3bcb4db48a3a2902b91
jffifa <jffifa@gmail.com>
2015-03-31 23:38:26 +0800

    modified

--- kotoridb/management/commands/color_print.py
+++ kotoridb/management/commands/color_print.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 
 def warn_print(s):
-    print ('\033[93m%s\033[0m' % (s,))
+    sys.stderr.write ('\033[93m%s\033[0m\n' % (s,))
 
 def fail_print(s):
-    print ('\033[91m%s\033[0m' % (s,))
+    sys.stderr.write ('\033[91m%s\033[0m\n' % (s,))
 
 def info_print(s):
-    print ('\033[92m%s\033[0m' % (s,))
+    sys.stdout.write ('\033[92m%s\033[0m\n' % (s,))
 
