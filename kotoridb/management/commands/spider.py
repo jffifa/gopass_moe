e9125c9539f2e9a6603e5771b38d68faf9d2f057
jffifa <jffifa@gmail.com>
2016-01-06 00:44:55 +0800

    modified

--- kotoridb/management/commands/spider.py
+++ kotoridb/management/commands/spider.py
@@ -5,11 +5,15 @@ import requests
 from pyquery import PyQuery as pq
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
+from kotoridb.models import Staff
 import urllib
+import pytz
 
 
 class Command(BaseCommand):
     scrape_url_prefix = 'http://cal.syoboi.jp/quarter'
+    place_trans = Staff.get_place_trans()
+    tz = pytz.timezone('Asia/Tokyo')
 
     @classmethod
     def gen_scrape_url_suffix(cls, time):
@@ -79,10 +83,45 @@ class Command(BaseCommand):
 
     @classmethod
     def get_cv_list(cls, anime_q):
+        res = []
         cast_char_q = anime_q('div.comment dl.cast > dt')
         cast_q = anime_q('div.comment dl.cast > dd.staffkeywords')
+
         for char, cast in [(pq(x), pq(y)) for (x, y) in zip(cast_char_q, cast_q)]:
-            print(char, cast)
+            char_name = char.text().strip()
+            cast_name = cast.text().strip()
+            res.append({'char_name':char_name,'cv_name':cast_name})
+
+        return res
+
+    @classmethod
+    def get_staff_list(cls, anime_q):
+        res = []
+        staff_title_q = anime_q('div.comment dl.staff > dt')
+        staff_q = anime_q('div.comment dl.staff > dd.staffkeywords')
+
+        for staff_title, staff in [(pq(x), pq(y)) for (x, y) in zip (staff_title_q, staff_q)]:
+            staff_title_name = staff_title.text().strip()
+            staff_name = staff.text().strip()
+            if staff_title_name == 'アニメーション制作':
+                continue
+
+            # split staff_title:
+            title_list = [x.strip() for x in staff_title_name.split('・')]
+            # split staff name
+            name_list = [x.strip() for x in staff_name.split('、')]
+
+            for title in title_list:
+                if title not in cls.place_trans:
+                    continue
+                for name in name_list:
+                    res.append({'staff_title':cls.place_trans(title), 'staff_name':name})
+
+        return res
+
+    @classmethod
+    def get_on_air_list(cls, anime_q):
+
 
     def handle(self, *args, **options):
         scrape_url = '/'.join((
@@ -116,4 +155,7 @@ class Command(BaseCommand):
             anime['title_cn'] = self.guess_anime_title_cn(anime['title'])
             anime['homepage'] = self.get_homepage(anime_q)
             anime['studio_list'] = self.get_studio_list(anime_q)
+            anime['cv_list'] = self.get_cv_list(anime_q)
+            anime['staff_list'] = self.get_staff_list(anime_q)
+            anime_list.append(anime)
 
